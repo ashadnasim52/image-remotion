45518b6441a68ea9c50c4554467efad0cef9bd71
ashad <ashadnasim72@gmail.com>
2024-06-03 00:13:47 +0530

    changes

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,24 @@
 import { Still } from 'remotion';
 import { PreviewCard } from './PreviewCard';
+import { PreviewCardTwo } from './PreviewCardTwo';
 import { myCompSchema } from './PreviewCard';
 
 export const RemotionRoot: React.FC = () => {
 	return (
 		<>
+			<Still
+				id='PreviewCardTwo'
+				component={PreviewCardTwo}
+				width={1200}
+				height={627}
+				schema={myCompSchema}
+				defaultProps={{
+					subtitle: 'sdfssdfsfdfdsubtitle' as const,
+					title: 'Welcome to Remotion' as const,
+					description: 'Edit Video.tsx to change template' as const,
+					color: '#0B84F3' as const,
+				}}
+			/>
 			<Still
 				id='PreviewCard'
 				component={PreviewCard}
@@ -12,7 +26,7 @@ export const RemotionRoot: React.FC = () => {
 				height={627}
 				schema={myCompSchema}
 				defaultProps={{
-					subtitle: 'sdfssdfsfdfd' as const,
+					subtitle: 'sdfssdfsfdfdsubtitle' as const,
 					title: 'Welcome to Remotion' as const,
 					description: 'Edit Video.tsx to change template' as const,
 					color: '#0B84F3' as const,
